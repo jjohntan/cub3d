5e5c93e11ef5c735a1ee5bf2553f6e424b67899c
Jek John Tan <jetan@u82z01s03.42kl.edu.my>
2025-01-21 17:04:24 +0800

    handle valid_color

--- main.c
+++ main.c
@@ -6,12 +6,13 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 16:02:34 by jetan             #+#    #+#             */
-/*   Updated: 2025/01/20 20:24:49 by jetan            ###   ########.fr       */
+/*   Updated: 2025/01/21 17:01:13 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 #include <stdio.h>
+#include <fcntl.h>
 
 // int	check_char()
 // {
@@ -28,21 +29,53 @@
 // 	}
 // 	return (1);
 // }
-static void	check_extension(char *av)
+
+static int	valid_color(int *color)
 {
-	int	len;
+	int	i;
 	
-	len = ft_strlen(av) - 4;
-	if (len < 4 || ft_strncmp(&av[len], ".cub", 4) != 0)
-		ft_putstr_fd("Error\n invalid extension", 2);
+	i = 0;
+	while (i < 3)
+	{
+		if (color[i] < 0 || color[i] > 255)
+			return (0);
+		i++;
+	}
+	return (1);
 }
+
+// static void	check_extension(char *av)
+// {
+// 	int	len;
+	
+// 	len = ft_strlen(av) - 4;
+// 	if (len < 4 || ft_strncmp(&av[len], ".cub", 4) != 0)
+// 		ft_putstr_fd("Error\n invalid extension", 2);
+// }
+
+// int	reading_file(file)
+// {
+// 	int		fd;
+// 	char	*line;
+	
+// 	fd = open(file, O_RDONLY);
+// 	if (fd < 0)
+// 		return (0);
+// 	while (line = get_next_line(fd) != NULL)
+// 	{
+		
+// 	}
+// 	close(fd);
+// 	return (1);
+// }
 
 int main(int ac, char **av)
 {
 	// t_data	data;
 	(void)ac;
+	(void)av;
 	
 	// ft_bzero(&data, sizeof(t_data);
-	check_extension(av[1]);
-	
+	// check_extension(av[1]);
+	// reading_file();
 }
