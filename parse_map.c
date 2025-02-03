606b603604d01606165950a9029e705a86e09791
Jek John Tan <jetan@u90z02s02.42kl.edu.my>
2025-02-03 21:16:10 +0800

    update check char function

--- parse_map.c
+++ parse_map.c
@@ -6,9 +6,30 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 14:56:50 by jetan             #+#    #+#             */
-/*   Updated: 2025/01/24 14:57:05 by jetan            ###   ########.fr       */
+/*   Updated: 2025/02/03 20:38:43 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+void	check_char(char **map)
+{
+	int	h;
+	int	w;
+	
+	h = 0;
+	while (map[h])
+	{
+		w = 0;
+		while (map[h][w])
+		{
+			if (map[h][w] != '0'&& map[h][w] != '1' && map[h][w]!= 'N' && map[h][w]!= 'S' && map[h][w]!='E' && map[h][w]!= 'W' && map[h][w] != 'D')
+			{
+				ft_putstr_fd("Error\nInvalid character in map", 2);
+				return ;
+			}
+			w++;
+		}
+		h++;
+	}
+}
