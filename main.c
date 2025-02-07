62d160ef93dc1d414bbbceb9b83f66a3afc91184
Jek John Tan <jetan@u82z01s05.42kl.edu.my>
2025-02-07 15:05:52 +0800

    add valid_player

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 16:02:34 by jetan             #+#    #+#             */
-/*   Updated: 2025/01/24 15:34:38by jetan            ###   ########.fr       */
+/*   Updated: 2025/02/07 15:04:37 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,13 +58,13 @@ void	valid_texture(char *file)
 		printf("hello");
 }
 
-void	count_players(char **map)
+int	count_players(char **map)
 {
 	int	h;
 	int	w;
-	char	current;
 	int	player_count;
 	
+	player_count = 0;
 	h = 0;
 	while (map[h])
 	{
@@ -74,46 +74,72 @@ void	count_players(char **map)
 			if (ft_strchr("NSEW", map[h][w]))
 			{
 				player_count++;
-				
-				return ;
+				// ->y = h;
+				// ->x = w;
+				// ->angle = map[h][w];
 			}
 			w++;
 		}
 		h++;
 	}
+	return (player_count);
+}
+
+void	valid_player(char **map)
+{
+	char	player_count;
+	
+	player_count = count_players(map);
+	if (player_count == 0)
+	{
+		ft_putstr_fd("Error\nNo player starting position found", 2);
+		return ;
+	}
+	if (player_count > 1)
+	{
+		ft_putstr_fd("Error\nMutiple player starting position found", 2);
+		return;
+	}
 }
 
 int main(int ac, char **av)
 {
-	// t_data	data;
+	// t_game	data;
 	(void)ac;
 	(void)av;
 	
-	 char row0[] = "111111";
-	   char row1[] = "100101";
-	   char row2[] = "101001";
-	   char row3[] = "1100N1";
-	   char row4[] = "111111";
-	   char *map[] = { row0, row1, row2, row3, row4, NULL };
-	// int i = 0;
-	// while (map[i])
-	// {
-	// 	printf("%s\n", map[i]);
-	// 	i++;
-	// }
-	// ft_bzero(&data, sizeof(t_data);
+	
+	char row0[] = "       1    ";
+	char row1[] = "      101   ";
+	char row2[] = "     10001  ";
+	char row3[] = "    100N001 ";
+	char row4[] = "   111111111";
+	char *map[] = { row0, row1, row2, row3, row4, NULL };
+	//  char row0[] = "111111";
+	//    char row1[] = "100101";
+	//    char row2[] = "101001";
+	//    char row3[] = "1100N1";
+	//    char row4[] = "111111";
+	//    char *map[] = { row0, row1, row2, row3, row4, NULL };
+	int i = 0;
+	while (map[i])
+	{
+		printf("%s\n", map[i]);
+		i++;
+	}
+	// ft_bzero(&data, sizeof(t_game);
 	// check_extension(av[1]);
+	// parse_map(av[1]);
+	// valid_map(map);
 	// check_walls(map);
 	// check_char(map);
-	// flood_fill(map, 4, 3);
-	// valid_map(map);
+	// flood_fill(map, 5, 3);
 	// valid_texture(av[1]);
-	// parse_map(av[1]);
-	// i = 0;
-	// while (map[i])
-	// {
-	// 	printf("%s\n", map[i]);
-	// 	i++;
-	// }
-	valid_player_position(map);
+	i = 0;
+	while (map[i])
+	{
+		printf("%s\n", map[i]);
+		i++;
+	}
+	// valid_player(map);
 }
